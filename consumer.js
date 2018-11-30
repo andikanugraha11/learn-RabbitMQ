ef29b7eef58a600368949f791ec046e6ef06d404
Andika Nugraha <m.andika.nugraha@gmail.com>
2018-12-01 00:21:36 +0700

    bug fixes & consumer

---
+++ consumer.js
@@ -0,0 +1,18 @@
+const amqp = require('amqplib');
+
+amqp.connect('amqp://localhost')
+    .then(conn => {
+        return conn.createChannel()
+            .then(ch => {
+                const ok = ch.assertExchange('hello', {durable:false}) // Queue declaration
+
+                ok.then(()=>{
+                    return ch.consume('hello', msg => {
+                        console.log('- Received', msg.content.toString())
+                    },{ noAck: true })
+                })
+                .then(() => {
+                    console.log('* Waiting for messages. Ctrl+C to exit')
+                })
+            })
+    }).catch(console.warn)
--- producer.js
+++ producer.js
@@ -3,11 +3,11 @@ const amqp =  require('amqplib');
 amqp.connect('amqp://localhost')
     .then(conn => {
         return conn.createChannel()
-            .then(ch =>{
+            .then(ch => {
                 const q = 'hello' //queue name is 'hello'
                 const msg = 'Hello World' // the message send to RabbitMQ
 
-                const ok = ch.assertExchange(q, {durable: false}) //create queue
+                const ok = ch.assertQueue(q, { durable: false }) //create queue
 
                 return ok.then(()=>{
                     ch.sendToQueue(q,Buffer.from(msg)) // sending message to RabbitMQ
